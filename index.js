ddc8b5c3d96590b0f79890d52260ec0443d190fe
Athira V Babu <athiravbabu3@gmail.com>
2023-06-20 01:26:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/notification", (req, res) => {
+  res.render("notification.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
